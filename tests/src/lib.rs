43e5cacd962edcacb5c9422294a7c7fe7b415099
+merlan #flirora <uruwi@protonmail.com>
2024-08-06 16:18:15 -0400

    Finish benchmark implementation

---
+++ tests/src/lib.rs
@@ -0,0 +1,13 @@
+use args::CliArguments;
+use clap::Parser;
+use once_cell::sync::Lazy;
+
+pub mod args;
+pub mod collect;
+pub mod constants;
+pub mod logger;
+pub mod run;
+pub mod world;
+
+/// The parsed command line arguments.
+pub static ARGS: Lazy<CliArguments> = Lazy::new(CliArguments::parse);
